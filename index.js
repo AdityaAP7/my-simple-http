3b621226876e35ecab4f40c25d6fd7623d0ff2e3
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-28 23:17:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,3 +66,4 @@ class MySimpleHTTP {
             // Optionally, you could reject if non-2xx:
             // reject(new Error(`Request failed with status code ${res.statusCode}`));
             // or just resolve to handle it outside
+            resolve(response);
