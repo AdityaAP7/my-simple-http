c66ff1ef465ee60113003b18e45b7adf57398edd
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-30 23:30:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,4 @@ class MySimpleHTTP {
         hostname: parsedURL.hostname,
         port: parsedURL.port || (parsedURL.protocol === 'https:' ? 443 : 80),
         path: parsedURL.path,
+        method: finalConfig.method,
