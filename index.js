ab80a610ef4debf60debeeb0b206992db70feb39
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-30 23:21:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const https = require('https');
 const http = require('http');
 const urlModule = require('url');
 class MySimpleHTTP {
+ /**
+   * @param {String} [baseURL=''] - Base URL for all requests
+   * @param {Object} [options={}] - Additional options
+   * @param {Object} [options.defaultHeaders={}] - Headers to send with every request
+   * @param {Number} [options.timeout=0] - Timeout for requests in milliseconds (0 = no timeout)
+   * @param {Array<Function>} [options.requestInterceptors=[]] - Functions to intercept request configs
+   * @param {Array<Function>} [options.responseInterceptors=[]] - Functions to intercept responses
+   */
