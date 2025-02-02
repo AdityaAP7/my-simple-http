7f93326016a47e6d5e58888567ab9e6211bfc3a5
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-02 23:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -179,3 +179,4 @@ return this.request({ ...config, method: 'PATCH', url, data });
 (async () => {
   // Create an instance with default headers and a timeout
   const httpClient = new MySimpleHTTP('https://jsonplaceholder.typicode.com', {
+    defaultHeaders: { 'X-Custom-Header': 'MySimpleHTTP' },
