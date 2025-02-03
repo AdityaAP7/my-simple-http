e84ee9bdfc9e5afe31e078075cffec3775c3ccc8
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-03 23:34:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -212,4 +212,5 @@ return this.request({ ...config, method: 'PATCH', url, data });
         const queryResponse = await httpClient.getWithParams('/posts', { userId: 1 });
     console.log('GET with params data:', queryResponse.data);
   } catch (error) {
+    console.error('Request Error:', error);
 
