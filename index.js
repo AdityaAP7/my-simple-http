0b464e863c04f03de9a849bf6dc017ea9d8e4612
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-04 23:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -172,49 +172,6 @@ return this.request({ ...config, method: 'PATCH', url, data });
    return this.get(fullURL, config);
   }
 }
-// ---------------------------
-// Example usage:
-// ---------------------------
-/*
-(async () => {
-  // Create an instance with default headers and a timeout
-  const httpClient = new MySimpleHTTP('https://jsonplaceholder.typicode.com', {
-    defaultHeaders: { 'X-Custom-Header': 'MySimpleHTTP' },
-    timeout: 5000, // 5s
-    // Log config before request
-    requestInterceptors: [
-      async (config) => {
-        console.log('Request interceptor:', config.method, config.url);
-        return config;
- },
-    ],
-    // Log response status after response
-    responseInterceptors: [
-      async (response) => {
-        console.log('Response interceptor:', response.status);
-        return response;
-  },
-    ],
-  });
 
-  try {
-    // GET request
-    const response = await httpClient.get('/posts/1');
-    console.log('GET Response data:', response.data);
-    // POST request
-    const postResponse = await httpClient.post('/posts', {
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
-});
-    console.log('POST Response data:', postResponse.data);
-    // GET with query params
-        const queryResponse = await httpClient.getWithParams('/posts', { userId: 1 });
-    console.log('GET with params data:', queryResponse.data);
-  } catch (error) {
-    console.error('Request Error:', error);
- }
-})();
-*/
 module.exports = MySimpleHTTP;
 
