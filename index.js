34d739f05cf1c3b14263a022112cfe2128940920
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-04 23:50:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const querystring = require('querystring');
 const fs = require('fs');
 const { URL } = require('url');
 class MySimpleHTTP {
+ /**
+   * @param {String} [baseURL=''] - Base URL for all requests
+   * @param {Object} [options={}] - Additional options
+   * @param {Object} [options.defaultHeaders={}] - Headers to send with every request
+   * @param {Number} [options.timeout=0] - Timeout for requests in milliseconds (0 = no timeout)
+   * @param {Array<Function>} [options.requestInterceptors=[]] - Functions to intercept request configs
+   * @param {Array<Function>} [options.responseInterceptors=[]] - Functions to intercept responses
+   * @param {Number} [options.maxRetries=3] - Maximum number of retries on failure
+   * @param {Number} [options.retryDelay=1000] - Delay (in ms) between retries
+   */
