a30bc7772e15d43901e56964a735f46b56a98017
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-09 23:40:12 +0530

    Update newtest.js

--- newtest.js
+++ newtest.js
@@ -5,3 +5,4 @@ async function testClient() {
   const client = new MySimpleHTTP('https://jsonplaceholder.typicode.com');
   try {
     // Example: GET request
+    const response = await client.get('/posts/1');
