3cfaddb67a8de15f33f4ebd1b73efe93fb4f6633
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-09 23:40:36 +0530

    Update newtest.js

--- newtest.js
+++ newtest.js
@@ -8,3 +8,4 @@ async function testClient() {
     const response = await client.get('/posts/1');
     const response = await client.get('/posts/1');
     console.log('GET /posts/1 Response:', response.data);
+    // Example: POST request
