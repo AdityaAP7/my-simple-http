6a9fb2aeb9228eea7b48222f20abb11798b6a6ac
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-26 23:47:46 +0530

    Update newtest.js

--- newtest.js
+++ newtest.js
@@ -30,3 +30,4 @@ async function testClient() {
     console.log('PUT /posts/1 Response:', updateResponse.data);
     console.log('--- Testing PATCH ---');
     const patchResponse = await client.patch('/posts/1', {
+      title: 'patched-title',
