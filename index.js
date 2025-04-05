177b5c036e7caeb08795ef57cc3b6841c488073b
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-05 23:32:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-apoiiiuytttrrewq1xzlkjhgfdsaiuytrewqew1        'Content-Type': 'multipart/form-data',
+sapoiiiuytttrrewq1xzlkjhgfdsaiuytrewqew1        'Content-Type': 'multipart/form-data',
 29     ...config,    return this.post(url, formData, {
 31    const formData = new FormData();
 47   * Utility for basic authentication      headers: {
