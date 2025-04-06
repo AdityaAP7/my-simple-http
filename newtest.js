34ac16a288b647446e7b7b4a3fa29c2e23d80fbc
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-06 23:40:08 +0530

    Update newtest.js

--- newtest.js
+++ newtest.js
@@ -1,4 +1,4 @@
-wq321987654321987654321const MySimpleHTTP = require('./index');
+ewq321987654321987654321const MySimpleHTTP = require('./index');
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 const client = new MySimpleHTTP(BASE_URL);
 async function testClient() {
