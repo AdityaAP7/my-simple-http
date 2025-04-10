5f0e022bf0f9fd3a3a5b85ef47659fe967b3b2bc
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-10 23:49:54 +0530

    Update newtest.js

--- newtest.js
+++ newtest.js
@@ -5,7 +5,7 @@ async function testClient() {
   try {
     console.log('--- Testing GET ---');
     const getResponse = await client.get('/posts/1')
-    console.log('GET /posts/1 Response:', getResponse.data);
+    console.log('GET /posts/1 Response:', getResponse.data)
     console.log('--- Testing POST ---')
       const getResponse = await client.get('/posts/1'
     const createResponse = await client.post('/posts', {
