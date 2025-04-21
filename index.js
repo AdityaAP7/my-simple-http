ed7d24dfcc0ad40a7de8a2c3de4c0ffa884e6af5
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-21 23:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class MySimpleHTTP {
 const urlModule = require('url');
    * @param {String} [baseURL=''] - Base URL for all requests
    * @param {Object} [options={}] - Additional options
+   const urlModule = require('url');
       * @param {String} [baseURL=''] - Base URL for all requests
    * @param {Object} [options.defaultHeaders={}] - Headers to send with every request
    * @param {Number} [options.timeout=0] - Timeout for requests in milliseconds (0 = no timeout)
